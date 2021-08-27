2ccf2ceb32eba305563229c13196185638f027ab
Ankit Gupta <58206597+devankit01@users.noreply.github.com>
2021-08-27 09:10:37 +0530

    Update app.py

--- Flask Ep04/app.py
+++ Flask Ep04/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_mail import Mail,Message
+from flask_mail import Mail,Message # pip install flask_mail
 
 app = Flask(__name__) #Initialise app
 
@@ -7,8 +7,8 @@ app = Flask(__name__) #Initialise app
 # Config
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'noreplydeveloperhub@gmail.com'
-app.config['MAIL_PASSWORD'] = 'devhub@987654321'
+app.config['MAIL_USERNAME'] = 'yourmail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'yourpassword' # On Allow less secure app in gmail manage settings
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app) # Instance
@@ -22,7 +22,7 @@ def result():
         subject = request.form['subject']
         message = request.form['message']
 
-        msg = Message(subject, sender = 'noreplydeveloperhub@gmail.com' , recipients = [email])
+        msg = Message(subject, sender = 'yourmail@gmail.com' , recipients = [email])
         msg.body = message
         mail.send(msg)
         return render_template('index.html', sent = True)
